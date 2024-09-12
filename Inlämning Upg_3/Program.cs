6948a1326e0903c820247238624f6b87f6436db3
karamTahhan <kata07001@utb.vaxjo.se>
2024-09-12 14:27:51 +0200

    Add project files.

---
+++ Inlämning Upg_3/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace upg_3
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+          Console.WriteLine("     /\\");
+          Console.WriteLine("    /  \\");
+          Console.WriteLine("   /\"\"  \\");
+          Console.WriteLine("  /______\\");
+
+        }
+
+
+
+
+
+    }
+
+
+
+
+
+
+
+
+}
